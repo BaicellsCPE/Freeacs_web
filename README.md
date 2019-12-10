14c8d175980a1f353f7e6b6e0e50119473ab8574
liuxianggui <liuxianggui@baicells.com>
2019-12-10 19:36:35 +0800

    update: 修改整体风格
    根据国家电网网站风格，修改本网页整体风格。
    1. 配色修改为绿色
    2. 增加logo、
    3. 修改系统名称
    
    Signed-off-by: liuxianggui <liuxianggui@baicells.com>

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # vue-element
 >Vue2.0+Vue-router+ElementUI
 
@@ -7,3 +8,24 @@ npm install
 npm run dev
 npm run build
 ```
+=======
+# free
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+>>>>>>> init
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+<<<<<<< HEAD
 import ElementUI from 'element-ui'
 // import '../theme/index.css'
 import 'element-ui/lib/theme-default/index.css'
@@ -15,3 +16,14 @@ const router = new VueRouter({routes: routerMap})
 const app = new Vue({
   router
 }).$mount('#app');
+=======
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
+>>>>>>> init
